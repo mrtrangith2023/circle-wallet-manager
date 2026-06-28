d164ecd47525526d29c4992e04ca9596c12be0fb
mrtrangith2023 <mrtranairdrop@gmail.com>
2026-06-28 11:45:11 +0700

    Day 01 - Add health and version endpoints

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="Circle Wallet Manager",
-    version="0.1.0"
+    title="Circle Wallet Manager API",
+    version="0.1.1"
 )
 
 @app.get("/")
 def root():
     return {
         "message": "Circle Wallet Manager API is running!"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status" : "healthy"
+    }
+
+@app.get("/version")
+def version():
+    return {
+        "version" : "0.1.1"
     }
